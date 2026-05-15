f6beb6255973cc110aaa41e3bcbd71af87fd0a1e
Bonecrusher <charlie@localhost>
2026-05-16 00:08:14 +1000

    Align app icon and version to 0.6.2 build 8

--- DubScribe/UI/SettingsView.swift
+++ DubScribe/UI/SettingsView.swift
@@ -311,7 +311,7 @@ struct SettingsView: View {
                                         Text("DubScribe")
                                             .font(.system(size: 14, weight: .bold))
                                             .foregroundColor(.white)
-                                        Text("Version 0.5.1")
+                                        Text("Version 0.6.2")
                                             .font(.system(size: 11))
                                             .foregroundColor(.white.opacity(0.5))
                                     }
