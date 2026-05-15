04573e19f8df4e01d2c9603eaac4a0b4db3f8cc0
Bonecrusher <charlie@localhost>
2026-05-15 23:36:58 +1000

    Prepare DubScribe 0.6.2 icon refresh

--- DubScribe/UI/SettingsView.swift
+++ DubScribe/UI/SettingsView.swift
@@ -326,7 +326,7 @@ struct SettingsView: View {
                                         Text("DubScribe")
                                             .font(.system(size: 14, weight: .bold))
                                             .foregroundColor(.white)
-                                        Text("Version 0.6.1")
+                                        Text("Version 0.6.2")
                                             .font(.system(size: 11))
                                             .foregroundColor(.white.opacity(0.5))
                                     }
